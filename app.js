97dfecd864cf169c5895c782e8e8d92e2fc13367
Mary Martinez <mary.diana.martinez@gmail.com>
2022-01-24 16:34:57 -0800

    Add function to handle Guesses

--- app.js
+++ app.js
@@ -14,26 +14,55 @@ const totalEl = document.getElementById('total');
 let winCount = 0;
 let totalCount = 0;
 
-const choices = ['shell 1', 'shell 2', 'shell 3'];
+const choices = ['shell1', 'shell2', 'shell3'];
 // set event listeners 
 
 shell1Button.addEventListener('click', () =>{
-  const Location = Math.floor(Math.random * 3);
-  const ballLocation = choices[Location];
-  handleGuess('Shell 1', ballLocation);
+    const location = Math.floor(Math.random() * 3);
+    const ballLocation = choices[location];
+    console.log('Button 1', ballLocation);
+    handleGuess('shell1', ballLocation);
 });
 
 shell2Button.addEventListener('click', () =>{
-  const Location = Math.floor(Math.random * 3);
-  const ballLocation = choices[Location];
-  handleGuess('Shell 2', ballLocation);
+    const location = Math.floor(Math.random() * 3);
+    const ballLocation = choices[location];
+    console.log('Button 2', ballLocation);
+    handleGuess('shell2', ballLocation);
 });
 
 shell3Button.addEventListener('click', () =>{
-  const Location = Math.floor(Math.random * 3);
-  const ballLocation = choices[Location];
-  handleGuess('Shell 3', ballLocation);
+    const location = Math.floor(Math.random() * 3);
+    const ballLocation = choices[location];
+    console.log('Button 3', ballLocation);
+    handleGuess('shell3', ballLocation);
 });
   // get user input
   // use user input to update state 
+
+function handleGuess(userGuess, answer) {
+    // reset classList
+    shell1Container.classList.remove('ball');
+    shell2Container.classList.remove('ball');
+    shell3Container.classList.remove('ball');
+    // update total count
+    totalCount++;
+    // check if userGuess === answer & update winCount if needed
+    if (userGuess === answer){
+        winCount++;
+    }
+    // if, else if, else statements to show the actual location of the ball
+    if (answer === 'shell1'){
+        shell1Container.classList.add('ball');
+    } else if (answer === 'shell2') {
+        shell2Container.classList.add('ball');
+    } else {
+        shell3Container.classList.add('ball');
+    }
+
+    //update counters in DOM to reflect new state
+    totalEl.textContent = totalCount;
+    winsEl.textContent = winCount;
+    lossesEl.textContent = totalCount - winCount;
+}
   // update DOM to reflect the new state
